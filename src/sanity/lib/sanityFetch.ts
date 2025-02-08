2add7dec8eceff59fab1221275e1d03234caaa1d
vishalkrochwani <158709336+vishalkrochwani@users.noreply.github.com>
2025-02-08 15:23:33 +0500

    Delete src directory

--- src/sanity/lib/sanityFetch.ts
+++
@@ -1,5 +0,0 @@
-import {client } from "@/sanity/lib/client"
-
-export async function sanityFetch({query , param = {}}: {query: string , param?: any}) {
-    return await client.fetch(query, param)
-}
--- src/sanity/schemaTypes/index.ts
+++
@@ -1,7 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import product from './product'
-import order from './order'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [product , order],
-}
